80e0645510627daef4f026de4f6062f010d344a7
Gavin Webster <gwebster70@gmail.com>
2021-07-07 09:30:32 +1000

    second commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <ChitChat />
+  <br>
+  <MessageMe />
+  <br>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import ChitChat from './components/ChitChat.vue'
+import MessageMe from './components/MessageMe.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+    ChitChat,
+    MessageMe
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  img: center
+}
+</style>
---
+++ src/components/ChitChat.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-6 max-w-sm mx-auto bg-gray-100 rounded-xl shadow-md flex items-center space-x-4">
+    <div class="flex-shrink-0">
+      <img class="h-12 w-12" src="../assets/logo.png" alt="ChitChat Logo">
+    </div>
+    <div>
+      <div class="text-xl font-medium text-black">ChitChat</div>
+      <p class="text-gray-500">You have a new message!</p>
+    </div>
+  </div>
+</template>
+<script lang="ts"></script>
+<style></style>
